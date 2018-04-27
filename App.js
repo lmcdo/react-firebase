7baec8b4f7710c39dc96b70359b612258550b16b
Nasir Sattar <lawrence.mcdonell@gmail.com>
2018-04-27 16:47:12 +1000

    firebase setup and listitems added and deleted ok

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ListView } from 'react-native';
 import * as firebase from 'firebase';
+import { Container, Content, Header, Form, Input, Item, Button, Label, Icon, List, ListItem } from 'native-base';
+
+
 
 const newLocal = "https://react-firebase-5c49c.firebaseio.com";
 // Initialise Firebase
@@ -14,27 +17,63 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
+var data= []
+
 
 export default class App extends React.Component {
   
   constructor(props){
     super(props)
 
+    this.ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2})
+
     this.state = ({
       email:'',
-      password: ''
+      password: '',
+      listViewData: data,
+      newContact:""
     })
   }
+
+  addRow(data){
+
+    var key = firebase.database().ref('/contacts').push().key
+    firebase.database().ref('/contacts').child(key).set({ name: data })
+
+  }
+
+  async deleteRow(secId, rowId, rowMap, data){
+
+    await firebase.database().ref('contacts/'+data.key).set(null)
+
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    var newData = [...this.state.listViewData];
+    newData.splice(rowId, 1)
+    this.setState({ listViewData: newData}); 
+
+
+  }
+
+  showInformation(){
+
+
+
+
+  }
+
+
+
 
 
   componentDidMount(){
 
-      firebase.auth().onAuthStateChanged((user) => {
-        if (user != null){
-          console.log(user)
-        }
-      })
+     var that = this
+     firebase.database().ref('/contacts').on('child_added', function(data){
+
+        var newData = [...that.state.listViewData]
+        newData.push(data)
+        that.setState({listViewData : newData})
+     })
 
 
   }
@@ -92,7 +131,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Container style={styles.container}>
-        <Form>
+        {/* <Form>
           <Item floatingLabel >
             <Label> Email </Label>
             <Input
@@ -142,8 +181,52 @@ export default class App extends React.Component {
           <Text style={{ color: 'white'}}> Login With Facebook  </Text>
           </Button>
 
+          
 
-        </Form>
+
+        </Form> */}
+
+        <Header style={{ marginTop: StatusBar.currentHeight }}>
+        <Content>
+          <Item>
+            <Input
+              placeholder="Add Name"
+              onChangeText = {(newContact) => this.setState({newContact})}
+            />
+            <Button onPress={() => this.addRow(this.state.newContact)}>
+              <Icon name="add" />
+            </Button>
+          </Item>
+          </Content>
+        </Header>
+
+        <Content>
+            <List
+            enableEmptySections
+            dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+            renderRow={data=>
+            <ListItem>
+              <Text>{data.val().name}</Text>
+            </ListItem>
+            }
+
+            renderLeftHiddenRow={data =>
+            <Button full onPress={()=> this.addRow(data)}>
+            <Icon name="information-circle"/>
+            </Button>
+            }
+
+            renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+              <Button full danger onPress={()=>this.deleteRow(secId, rowId, rowMap,data)}>
+              <Icon name="trash"/>
+              </Button>
+              }
+
+              leftOpenValue={-75}
+              rightOpenValue={-75}
+
+            />
+          </Content>
       </Container>
         
     );
